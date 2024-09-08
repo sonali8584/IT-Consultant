3bebef1a174aca2c982ec8693d7b58c5ec1ababf
sonali8584 <jaiswalsonali616@gmail.com>
2024-09-09 00:12:00 +0530

    text increase

---
+++ web.js
@@ -0,0 +1,20 @@
+function getname(){
+    console.log("web development");
+    console.log("sonali jaiswal");
+}
+getname();
+let name1="development"
+const name2="graphic designer"
+var name3="animator"
+function getdata(){
+console.log(name1);
+console.log(name2);
+console.log(name3);
+}
+getdata();
+function getidea(){
+    let x="40"
+    console.log(x);
+}
+getidea();
+console.log(name2);
